f6ee76b306df6f518d838b40f135abf86e832d7f
Jesse <JesseJoeFlores@Gmail.com>
2016-12-06 22:09:22 -0600

    created madlibs with javascript

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var noun = prompt ("enter a noun.");
+var verb = prompt ("enter a verb.");
+var adj = prompt ("enter a adjective.");
+var adverb = prompt ("enter a adverb.");
+
+document.write("Do you " + verb + " your " + adj + " " + noun + " " + adverb + "? That's hilarious!");
